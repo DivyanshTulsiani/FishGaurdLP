7b12a0ed41c81fa6853ec1b5e69386556d3d1f2a
vivekYadav-asa <vy6751536@gmail.com>
2025-03-21 17:23:16 +0530

    added scroll and third image

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('scrollButton').addEventListener('click', function() {
+    let target = document.getElementById('workflow1start');
+    if (target) {
+        target.scrollIntoView({ behavior: 'smooth' });
+    }
+});
+ function addPrice(){
+    
+ }
